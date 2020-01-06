29624661cbe00ade3082b3e127cbac970f64c401
GeonWooPaeng <gwpaeng@naver.com>
2020-01-06 19:45:03 +0900

    백준 5397

--- 자료구조/backjun.py
+++ 자료구조/backjun.py
@@ -1,25 +1,53 @@
-case_test = int(input())
-for _ in range(case_test):
-    L = list(input())
-    stack_l = []
-    stack_r = []
+# 1.
+# case_test = int(input())
+# for _ in range(case_test):
+#     L = list(input())
+#     stack_l = []
+#     stack_r = []
 
-    for i in range(len(L)):
-        stack_l.append(L[i])
-        if stack_l[-1] == '<':
-            stack_l.pop()
-            if stack_l:
-                stack_r.append(stack_l.pop())
+#     for i in range(len(L)):
+#         stack_l.append(L[i])
+#         if stack_l[-1] == '<':
+#             stack_l.pop()
+#             if stack_l:
+#                 stack_r.append(stack_l.pop())
 
-        elif stack_l[-1] == '>':
-            stack_l.pop()
-            if stack_r:
-                stack_l.append(stack_r.pop())
+#         elif stack_l[-1] == '>':
+#             stack_l.pop()
+#             if stack_r:
+#                 stack_l.append(stack_r.pop())
         
-        elif stack_l[-1] == '-':
-            stack_l.pop()
-            if stack_l:
-                stack_l.pop()
+#         elif stack_l[-1] == '-':
+#             stack_l.pop()
+#             if stack_l:
+#                 stack_l.pop()
 
-    stack_l.extend(reversed(stack_r))
-    print(''.join(stack_l))
+#     stack_l.extend(reversed(stack_r))
+#     print(''.join(stack_l))
+
+
+# 2.
+# test_case = int(input())
+
+# for _ in range(test_case):
+#     stack_l = []
+#     stack_r = []
+#     data = input() 
+#     for i in data:
+#         if i == '-':
+#             if stack_l:
+#                 stack_l.pop()
+        
+#         elif i == '<':
+#             if stack_l:
+#                 stack_r.append(stack_l.pop())
+                
+#         elif i == '>':
+#             if stack_r:
+#                 stack_l.append(stack_r.pop())
+        
+#         else:
+#             stack_l.append(i)
+
+#     stack_l.extend(reversed(stack_r))
+#     print(''.join(stack_l))
