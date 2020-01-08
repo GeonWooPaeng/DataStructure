86af6142d4dbfb526be75ecf5d8cb1ceb52e2c62
GeonWooPaeng <gwpaeng@naver.com>
2020-01-08 21:09:04 +0900

    백준

--- 자료구조/backjun.py
+++ 자료구조/backjun.py
@@ -246,6 +246,41 @@
 
 # print(a)
 
+# 1074.
+
+# def Z(n,x,y):
+#     global result 
+#     if n == 2:
+#         if x == X and y == Y:
+#             print(result)
+#             return 
+#         result += 1
+
+#         if x == X and y+1 == Y:
+#             print(result)
+#             return 
+#         result += 1
+
+#         if x+1 == X and y == Y:
+#             print(result)
+#             return 
+#         result += 1
+
+#         if x+1 == X and y+1 == Y:
+#             print(result)
+#             return 
+#         result += 1
+#         return
+
+#     Z(n/2,x,y)
+#     Z(n/2,x,y+n/2)
+#     Z(n/2,x+n/2,y)
+#     Z(n/2,x+n/2,y+n/2)
+
+# result = 0
+# N,X,Y = map(int,input().split(' '))
+# Z(2**N,0,0)
+
 
 
     
