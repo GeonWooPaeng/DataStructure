b9dc34273a91d6b9e4f34864bbc8ba21bc7432d1
GeonWooPaeng <gwpaeng@naver.com>
2020-01-16 17:40:00 +0900

    백준

--- 자료구조/backjun.py
+++ 자료구조/backjun.py
@@ -604,47 +604,106 @@
 #     if n == 1:
 #         return 1
 #     return pibo(n-1) + pibo(n-2)
-
 # n = int(input())
 # print(pibo(n))
 
-1991.
+# 1991.
+# class Node:
+#     def __init__(self,data,left_node,right_node):
+#         self.data = data 
+#         self.left_node = left_node 
+#         self.right_node = right_node 
+
+# def pre_order(node):
+#     print(node.data, end='')
+#     if node.left_node != '.':
+#         pre_order(tree[node.left_node])
+#     if node.right_node != '.':
+#         pre_order(tree[node.right_node])
+
+# def in_order(node):
+#     if node.left_node != '.':
+#         in_order(tree[node.left_node])
+#     print(node.data,end='')
+#     if node.right_node != '.':
+#         in_order(tree[node.right_node])
+
+# def post_order(node):
+#     if node.left_node != '.':
+#         post_order(tree[node.left_node])
+#     if node.right_node != '.':
+#         post_order(tree[node.right_node])
+#     print(node.data,end='')
+
+# n = int(input())
+# tree = {}
+
+# for i in range(n):
+#     data, left_node, right_node = input().split() 
+#     tree[data] = Node(data,left_node,right_node)
+
+# pre_order(tree['A'])
+# print()
+# in_order(tree['A'])
+# print()
+# post_order(tree['A'])
+
+2250.
 class Node:
-    def __init__(self,data,left_node,right_node):
-        self.data = data 
+    def __init__(self,number,left_node,right_node):
+        self.parent = -1 
+        self.number = number 
         self.left_node = left_node 
         self.right_node = right_node 
 
-def pre_order(node):
-    print(node.data, end='')
-    if node.left_node != '.':
-        pre_order(tree[node.left_node])
-    if node.right_node != '.':
-        pre_order(tree[node.right_node])
+def in_order(node,level):  #중위 순회로 좌측부터 검사 / level은 행 
+    global level_depth,x # level_depth는 행 개수
+    level_depth = max(level_depth, level)
+    if node.left_node != -1:
+        in_order(tree[node.left_node], level + 1) # 자식 node인 경우에는 level이 다르기 때문에 level + 1을 해준다
+    
+    level_min[level] = min(level_min[level], x) # 중위 순회로 해서 x+=1 해서 저장하는 이유는 비어있는 열이 없기 때문입니다.
+    level_max[level] = max(level_max[level], x)
+    x += 1
+    
+    if node.right_node != -1:
+        in_order(tree[node.right_node], level + 1) # 자식 node인 경우에는 level이 다르기 때문에 level + 1을 해준다
 
-def in_order(node):
-    if node.left_node != '.':
-        in_order(tree[node.left_node])
-    print(node.data,end='')
-    if node.right_node != '.':
-        in_order(tree[node.right_node])
-
-def post_order(node):
-    if node.left_node != '.':
-        post_order(tree[node.left_node])
-    if node.right_node != '.':
-        post_order(tree[node.right_node])
-    print(node.data,end='')
-
-n = int(input())
+n = int(input()) 
 tree = {}
+level_min = [n] #같은 level에서 최소의 열 저장
+level_max = [0] #같은 level에서 최대의 열 저장
+root = -1 
+x = 1
+level_depth = 1 
 
-for i in range(n):
-    data, left_node, right_node = input().split() 
-    tree[data] = Node(data,left_node,right_node)
+for i in range(1, n+1):
+    tree[i] = Node(i,-1,-1)
+    level_min.append(n)
+    level_max.append(0)
 
-pre_order(tree['A'])
-print()
-in_order(tree['A'])
-print()
-post_order(tree['A'])
+for _ in range(n): #tree에 node 저장 하기
+    number,left_node,right_node = map(int,input().split())
+    tree[number].left_node = left_node 
+    tree[number].right_node = right_node 
+    if left_node != -1:
+        tree[left_node].parent = number 
+    if right_node != -1:
+        tree[right_node].parent = number 
+
+for i in range(1, n+1): 
+    if tree[i].parent == -1: #부모 node가 없으면 root이다.
+        root = i
+
+in_order(tree[root], 1)
+
+result_level = 1
+result_width = level_max[1] - level_min[1] + 1  #결과 넓이
+for i in range(2, level_depth + 1): # 1은 이미 계산 했음
+    width = level_max[i] - level_min[i] + 1
+    if result_width < width: # 결과 넓이보다 넓을 경우
+        result_level = i
+        result_width = width 
+
+print(result_level, result_width)
+
