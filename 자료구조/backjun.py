00859dd52b6633a6ba899c8faadc900bfe6edc2d
GeonWooPaeng <gwpaeng@naver.com>
2020-01-20 23:04:00 +0900

    백준

--- 자료구조/backjun.py
+++ 자료구조/backjun.py
@@ -1018,3 +1018,24 @@
 # dfs(v)
 # visited = [False] * (n+1)
 # bfs(v)
+
+# 1697.
+
+# from collections import deque 
+
+# MAX = 100001 
+# n, k = map(int,input().split())
+# array [0] * MAX 
+
+# def bfs():
+#     q = deque([n])
+#     while q:
+#         now_pos = q.popleft()
+#         if now_pos == k:
+#             return array[now_pos]
+#         for next_pos in (now_pos -1, now_pos +1, now_pos *2):
+#             if 0 <= nest_pos < MAX and not array[next_pos]:
+#                 array[next_pos] = array[now_pos] + 1
+#                 q.append(next_pos)
+# print(dfs())
+
