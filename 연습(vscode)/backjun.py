38f4d933698b280e810f143a351ace94315040ac
GeonWooPaeng <gwpaeng@naver.com>
2020-02-05 22:19:03 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -1446,66 +1446,81 @@
 #     else:
 #         print(-1)
 
-1774.
-import math 
-import sys
-input = sys.stdin.readline 
+# 1774.
+# import math 
+# import sys
+# input = sys.stdin.readline 
 
-def get_distance(p1,p2):
-    a = p1[0] - p2[0]
-    b = p1[1] - p2[1] 
-    return math.sqrt((a * a) + (b * b))
+# def get_distance(p1,p2):
+#     a = p1[0] - p2[0]
+#     b = p1[1] - p2[1] 
+#     return math.sqrt((a * a) + (b * b))
 
-def get_parent(parent, n):
-    if parent[n] == n:
-        return n 
-    return get_parent(parent, parent[n])
+# def get_parent(parent, n):
+#     if parent[n] == n:
+#         return n 
+#     return get_parent(parent, parent[n])
 
-def union_parent(parent, a, b):
-    a = get_parent(parent, a)
-    b = get_parent(parent, b) 
-    if a < b:
-        parent[b] = a 
-    else:
-        parent[a] = b 
+# def union_parent(parent, a, b):
+#     a = get_parent(parent, a)
+#     b = get_parent(parent, b) 
+#     if a < b:
+#         parent[b] = a 
+#     else:
+#         parent[a] = b 
 
-def find_parent(parent, a, b):
-    a = get_parent(parent, a)
-    b = get_parent(parent, b)
-    if a == b:
-        return True 
-    else:
-        return False
+# def find_parent(parent, a, b):
+#     a = get_parent(parent, a)
+#     b = get_parent(parent, b)
+#     if a == b:
+#         return True 
+#     else:
+#         return False
 
-edges = []
-parent = {}
-locations = []
+# edges = []
+# parent = {}
+# locations = []
 
-n,m = map(int,input().split())
+# n,m = map(int,input().split())
 
-for _ in range(n):
-    x, y = map(int,input().split())
-    locations.append((x,y))
+# for _ in range(n):
+#     x, y = map(int,input().split())
+#     locations.append((x,y))
 
-length = len(locations)
+# length = len(locations)
 
-for i in range(length-1):
-    for j in range(i+1,length):
-        edges.append((i + 1, j + 1, get_distance(locations[i],locations[j])))
+# for i in range(length-1):
+#     for j in range(i+1,length):
+#         edges.append((i + 1, j + 1, get_distance(locations[i],locations[j])))
 
-for i in range(1, n+1):
-    parent[i] = i 
+# for i in range(1, n+1):
+#     parent[i] = i 
 
-for i in range(m):
-    a, b = map(int,input().split())
-    union_parent(parent, a, b)
+# for i in range(m):
+#     a, b = map(int,input().split())
+#     union_parent(parent, a, b)
 
-edges.sort(key=lambda data: data[2])
+# edges.sort(key=lambda data: data[2])
 
-result = 0 
-for a, b, cost in edges:
-    if not find_parent(parent, a, b):
-        union_parent(parent, a, b)
-        result += cost 
+# result = 0 
+# for a, b, cost in edges:
+#     if not find_parent(parent, a, b):
+#         union_parent(parent, a, b)
+#         result += cost 
 
-print("%0.2f" %result)
+# print("%0.2f" %result)
+
+# 5585.
+# wallet = [500,100,50,10,5,1]
+# cost = int(input())
+# rem_cost = 1000 - cost 
+# result_num = 0 
+
+# for money in wallet:
+#     if rem_cost >= money:
+#         mo_num = rem_cost // money 
+#         rem_cost -= money * mo_num 
+#         result_num += mo_num 
+
+# print(result_num)
+
