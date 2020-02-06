402acbdd11ac7ae5fd643327806a3577842dcd69
GeonWooPaeng <gwpaeng@naver.com>
2020-02-07 00:06:02 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -1524,3 +1524,109 @@
 
 # print(result_num)
 
+# 1439.
+# s = input() 
+
+# count_1 = 0
+# count_0 = 0
+
+# if s[0] == '1':
+#     count_0 += 1
+# else:
+#     count_1 += 1
+
+# for i in range(len(s)-1):
+#     if s[i] != s[i+1]:
+#         if s[i+1] == '1':
+#             count_0 += 1
+
+#         else:
+#             count_1 += 1
+
+# print(min(count_1,count_0))
+
+# 2012.
+# 1.
+# stu_num = int(input())
+
+# Rank = [False] * (stu_num + 1)
+# Rank[0] = True 
+# result = 0
+# over_rank = []
+
+# for i in range(1,stu_num+1):
+#     pre_rank = int(input())
+    
+#     if Rank[pre_rank] == False:
+#         Rank[pre_rank] = True 
+#         continue 
+
+#     over_rank.append(pre_rank)
+        
+# for rank in over_rank:
+#     for j in range(1,stu_num+1):
+#         if Rank[j] == False:
+#             Rank[j] = True 
+#             result += abs(rank - j)
+
+# print(result)
+
+# 2.
+# n = int(input())
+# array = []
+
+# for _ in range(n):
+#     array.append(int(input()))
+
+# array.sort()
+
+# result = 0
+# for i in range(1,len(array)+1):
+#     result += abs(i-array[i-1])
+# print(result)
+ㄴ
+1092. 배
+import sys 
+
+n = int(input())
+cranes = list(map(int,input().split()))
+
+m = int(input())
+boxes = list(map(int,input().split()))
+
+#모든 박스를 옮길 수 없는 경우
+if max(cranes) < max(boxes):
+    print(-1)
+    sys.exit()
+
+#각 크레인이 현재 옮겨야 하는 박스의 번호(0부터 시작)
+positions = [0] * n 
+
+#각 박스를 옮겼는지의 여부
+checked = [False] * m 
+
+#최적의 해를 구해야 하므로 , 내림차순 정렬 
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
+
+result = 0
+count = 0
+
+while True:
+    if count == len(boxes): #다 옮겼으면 종료
+        break 
+    for i in range(n): #모든 크레인에 대하여 각각 처리
+        while positions[i] < len(boxes):
+            #아직 안 옮긴 박스 중에서, 옮길 수 있는 박스를 만날때 까지 반복
+            if not checked[positions[i]] and cranes[i] >= boxes[positions[i]]:
+                checked[positions[i]] = True 
+                positions[i] += 1
+                count += 1 
+                break 
+            positions[i] += 1 
+    result += 1 
+
+print(result)
+
+
+
