082a09ecfcb3e901f398a83035a915f2857ce8cb
GeonWooPaeng <gwpaeng@naver.com>
2020-02-09 00:10:46 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -1654,3 +1654,36 @@
 
 # print(sum(dist_dif))
 
+1461. 
+import heapq
+
+n,m = map(int,input().split()) #책수,들수있는 책수
+posi = list(map(int,input().split()))
+
+positive = []
+negative = [] 
+
+posi.sort()
+largest = max(-posi[0], posi[-1]) 
+
+for i in posi:
+    if i > 0: 
+        heapq.heappush(positive,-i)
+    else:
+        heapq.heappush(negative,i)
+
+result = 0
+while positive:
+    result += heapq.heappop(positive)
+    for _ in range(m-1):
+        if positive:
+            heapq.heappop(positive)
+
+while negative:
+    result += heapq.heappop(negative)
+    for _ in range(m-1):
+        if negative:
+            heapq.heappop(negative)
+
+print(-result*2 - largest)
+
