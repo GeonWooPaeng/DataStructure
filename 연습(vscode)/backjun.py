ff828cb6041e68d36bfe49cffb948c3981a6813c
GeonWooPaeng <gwpaeng@naver.com>
2020-02-12 14:18:24 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -1976,6 +1976,27 @@
 #     if count >= 1 and count <= l-2:
 #         print('',join(password))
 
+# 17269.
 
+# board = {'A':3,'B':2,'C':1,'D':2,'E':4,'F':3,'G':1,'H':3,'I':1,'J':1,'K':3,'L':1,'M':3,'N':2,'O':1,'P':2,'Q':2,'R':2,'S':1,'T':2,'U':1,'V':1,'W':1,'X':2,'Y':2,'Z':1}
 
+# n,m = map(int,input().split()) #이름 길이 
+# name1, name2 = list(input().split())
 
+# min_num = min(n,m)
+
+# string = ''
+
+# for i in range(min_num):
+#     string += name1[i] + name2[i]
+
+# string += name1[min_num:] + name2[min_num:]
+# string = string.upper() 
+
+# num = [board[i] for i in string]
+
+# for i in range(n+m-2):
+#     for j in range(n+m-1-i):
+#         num[j] += num[j+1]
+
+# print('{}%'.format((num[0]%10)*10 + num[1]%10))
