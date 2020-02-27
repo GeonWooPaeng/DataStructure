fcb8b1ea23e7dd871933e349671361164b0d169d
GeonWooPaeng <gwpaeng@naver.com>
2020-02-27 19:04:49 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -2620,6 +2620,47 @@
 #     print(ans)
 
 
+# 1932.
+
+# n = int(input())
+# #dp[i][j] : i,j 도착했을 때 최댓값 
+# #dp[i][j] = max(dp[i-1][j-1],dp[i-1][j]) + a[i][j]
+
+# a = [[0 for _ in range(n+1)] for i in range(n+1)]
+# dp = [[0 for _ in range(n+1)] for i in range(n+1)]
+
+
+# for i in range(1,n+1):
+#     tmp = list(map(int,input().split()))
+#     for j in range(1,i+1):
+#         a[i][j] = tmp[j-1]
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         dp[i][j] = max(dp[i-1][j-1],dp[i-1][j]) + a[i][j]
+
+# print(max(dp[-1]))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
