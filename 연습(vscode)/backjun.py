f8e6a24ae24263b9aa5c32696d5a5b50051ab39e
GeonWooPaeng <gwpaeng@naver.com>
2020-03-06 21:39:05 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -3068,5 +3068,40 @@
 
 # print((tot+1)//2)
 
+# 12849.
+# # 0: 정보과학관 
+# # 1: 전산관
+# # 2: 미래관
+# # 3: 신양관
+# # 4: 한경직기념관
+# # 5: 진리관
+# # 6: 학생회관
+# # 7: 형남공학관
+
+# #dp는 0분에 어떤 지정장소에 도착할 수 있는 상태(정보과학관에서 각 도착지점까지의 경로수)
+# dp = [1, 0, 0, 0, 0, 0, 0, 0] 
+
+# #각 지점에 올 수 있는 경우는 각 지점의 이웃에서 1분 후에 오는 경우 
+# def nxt(state): # 1분뒤에 초기 지점(정보과학관) 부터 각 지점에 올 수 있는 경우
+# 	tmp = [0 for _ in range(8)]
+# 	tmp[0] = state[1] + state[2]
+# 	tmp[1] = state[0] + state[2] + state[3]
+# 	tmp[2] = state[0] + state[1] + state[3] + state[4]
+# 	tmp[3] = state[1] + state[2] + state[4] + state[5]
+# 	tmp[4] = state[2] + state[3] + state[5] + state[7]  
+# 	tmp[5] = state[3] + state[4] + state[6]
+# 	tmp[6] = state[5] + state[7]
+# 	tmp[7] = state[4] + state[6]
+
+# 	for i in range(8):
+# 		tmp[i] %= 1000000007  
+	
+# 	return tmp
 
 
+# n = int(input())
+
+# for i in range(n):
+# 	dp = nxt(dp)
+
+# print(dp[0])
