5862f65aa9e56d59fe4d77c94fda4a1c63af581c
GeonWooPaeng <gwpaeng@naver.com>
2020-03-16 22:46:01 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -3689,4 +3689,18 @@
 # for i in result:
 #     print(i)
 
+# 1003.
+# dp_0 = [1,0] 
+# dp_1 = [0,1] 
 
+# def fi(x):
+#     if len(dp_0) < x+1:
+#         for i in range(len(dp_0), x+1):
+#             dp_0.append(dp_0[i-1] + dp_0[i-2])
+#             dp_1.append(dp_1[i-1] + dp_1[i-2])
+#     print(dp_0[x], dp_1[x])
+
+# for _ in range(int(input())):
+#     fi(int(input()))
+
+    
