6c7f45b4b03e185dc1e0ba2d50d010a503098c8a
GeonWooPaeng <gwpaeng@naver.com>
2020-03-19 00:39:02 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -3784,22 +3784,21 @@
 
 # print(dfs(1))
 
-1436.
+# 1436.
 
-def cal(x):
-    
-    for i in range(2,x+1):
-        if i % 3 == 0:
-            dp[i] = min(dp[i//3], dp[i-1]) +1
+# def cal(x):
+#     for i in range(2,x+1):
+#         if i % 3 == 0:
+#             dp[i] = min(dp[i//3], dp[i-1]) +1
         
-        elif i % 2 == 0:
-            dp[i] = min(dp[i//2],dp[i-1])+1
+#         elif i % 2 == 0:
+#             dp[i] = min(dp[i//2],dp[i-1])+1
 
-        else:
-            dp[i] = dp[i-1] + 1
-    return dp[x]
+#         else:
+#             dp[i] = dp[i-1] + 1
+#     return dp[x]
 
 
-n = int(input())
-dp = [0 for _ in range(n+1)]
-print(cal(n))
+# n = int(input())
+# dp = [0 for _ in range(n+1)]
+# print(cal(n))
