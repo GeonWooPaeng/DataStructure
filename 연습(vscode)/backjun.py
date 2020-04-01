92ec11bc44388acf046d91ad228c383b32b7b456
GeonWooPaeng <gwpaeng@naver.com>
2020-04-01 22:41:38 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -4069,6 +4069,19 @@
 #         c[body].append(dress)
 #     print(cal(c))
 
+# 11726.
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input()) 
+# dp = [0 for _ in range(n)]
+# dp[0] = 1
+# dp[1] = 2
+
+# for i in range(2,n):
+#     dp[i] = dp[i-1] + dp[i-2]
+
+# print(dp[n-1]%10007)
 
 
 
