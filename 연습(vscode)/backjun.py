fcebb888f21322e05fd87fef95cc73bbd3802cd6
GeonWooPaeng <gwpaeng@naver.com>
2020-06-14 23:01:01 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -4461,8 +4461,47 @@
 #     print("<여러개 들어갔을 때>")
 #     program_page = Program(2)
 #     print("<1개 들어갔을 때>")
-#     program_page2 = Program(1)
+#     program_page2 = Program(1) 
     
+
+# 2193.
+n = int(input())
+dp = [0]*(n+1)
+dp[0] = 0
+dp[1] = 1
+for i in range(2, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+print(dp)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
