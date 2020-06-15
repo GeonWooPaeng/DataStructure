0b4e04f1eb2cb37b554f8a38d764a79f71dfff2b
GeonWooPaeng <gwpaeng@naver.com>
2020-06-15 09:57:54 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -4465,49 +4465,15 @@
     
 
 # 2193.
-n = int(input())
-dp = [0]*(n+1)
-dp[0] = 0
-dp[1] = 1
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
-print(dp)
+# n = int(input())
+# dp = [0]*(n+1)
+# dp[0] = 0
+# dp[1] = 1
+# for i in range(2, n+1):
+#     dp[i] = dp[i-1] + dp[i-2]
+# print(dp[n])
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
 
