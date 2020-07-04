56ad7d0b231251be15ad48eec1228cb68ecab9f0
GeonWooPaeng <gwpaeng@naver.com>
2020-07-05 00:39:52 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -4685,3 +4685,34 @@
 # 	alpha_array[alpha_idx]+=1
 
 # print(' '.join(map(str,alpha_array)))
+
+# 2875
+# import sys 
+# input = sys.stdin.readline
+# girl, boy, intership = map(int,input().split()) 
+
+# team_num = 0
+
+# while True:
+# 	if girl > 1 and boy > 0 and girl+boy > intership+2:
+# 		girl-=2
+# 		boy-=1
+# 		team_num+=1 
+# 	else:
+# 		break
+# print(team_num)	
+
+11052
+
+n = int(input())
+price = [0]+ list(map(int,input().split()))
+dp = [0] *(n+1)
+dp[1] = price[1]
+for i in range(2,n+1):
+    for j in range(1,i+1):
+        dp[i] = max(dp[i], dp[i-j]+price[j])
+
+print(dp[n])
+
+
+			
