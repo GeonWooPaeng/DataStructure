2ce025ae602d95f654b77473d85445c4429a3806
GeonWooPaeng <gwpaeng@naver.com>
2020-08-08 21:46:28 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -7358,26 +7358,26 @@
 
 
 # 12852
-n = int(input())
-dp = [[0,[]] for _ in range(n+1)]
+# import sys 
+# input = sys.stdin.readline 
 
-dp[1][0] = 0
-dp[1][1] = [1]
+# n = int(input())
+# dp = [[0,[]] for _ in range(n+1)]
+# dp[1][0] = 0
+# dp[1][1] = [1]
 
-for i in range(2,n+1):
-    if i % 3 == 0 and dp[i][0] > dp[i//3][0] + 1:
-        dp[i][0] = dp[i//3][0] + 1
-        dp[i][1] = dp[i//3][0] + [i]
+# for i in range(2,n+1):
+#     dp[i][0] = dp[i-1][0] + 1 
+#     dp[i][1] = dp[i-1][1] + [i]
 
-    if i % 2 == 0 and dp[i][0] > dp[i//2][0] + 1:
-        dp[i][0] = dp[i//2][0] + 1 
-        dp[i][1] = dp[i//2][1] + [i]
-    
-    else:
-        dp[i][0] = dp[i-1][0] + 1 
-        dp[i][1] = dp[i-1][1] + [i]
+#     if i % 3 == 0 and dp[i][0] > dp[i//3][0] + 1:
+#         dp[i][0] = dp[i//3][0] + 1 
+#         dp[i][1] = dp[i//3][1] + [i]
 
-print(dp[n][0])
-for j in dp[n][1][::-1]:
-    print(j, end = ' ')
+#     if i % 2 == 0 and dp[i][0] > dp[i//2][0] + 1:
+#         dp[i][0] = dp[i//2][0] + 1
+#         dp[i][1] = dp[i//2][1] + [i]
 
+# print(dp[n][0])
+# for j in dp[n][1][::-1]:
+#     print(j, end=' ')
