6329b0c682afa4c73df790b8953c6fcd1bb7e358
GeonWooPaeng <gwpaeng@naver.com>
2020-08-19 17:00:06 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -8017,3 +8017,49 @@
     
 # solution(0,0)
 # print(min(answer))
+
+# 2.
+# from itertools import combinations 
+
+# n,m = map(int,input().split())
+# chicken, house = [], [] 
+
+# for r in range(n):
+#     data = list(map(int,input().split()))
+#     for c in range(n):
+#         if data[c] == 1:
+#             house.append((r,c))
+#         elif data[c] == 2:
+#             chicken.append((r,c))
+
+# candidates = list(combinations(chicken, m))
+
+# def get_sum(candidate):
+#     result = 0 
+#     for hx, hy in house:
+#         temp = 1e9
+#         for cx, cy in candidate:
+#             temp = min(temp, abs(hx-cx)+abs(hy-cy))
+#         result += temp
+#     return result 
+
+# result = 1e9 
+# for candidate in cadidates:
+#     result = min(result, get_sum(candidate))
+
+# print(result)
+
+
+#1188
+# 최대공약수 
+# n, m = map(int,input().split())
+
+# def GCD(a,b):
+#     if a % b == 0:
+#         return b 
+#     return GCD(b, a%b)
+# if n == m:
+#     print(0)
+# else:
+#     gcd = GCD(n,m)
+#     print(m-gcd)
