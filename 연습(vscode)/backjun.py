8a80ab060762a5313739bad1eaed0a9333536e0a
GeonWooPaeng <gwpaeng@naver.com>
2020-10-14 15:52:36 +0900

    책

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -9970,8 +9970,44 @@
 # else:
 #     print(count)
 
+# 2.
+# from bisect import bisect_left, bisect_right 
+
+# def count_by_range(array, left_value, right_value):
+#     right_index = bisect_right(array, right_value)
+#     left_index = bisect_left(array, left_value)
+#     return right_index, left_index 
+
+# n,x = map(int,input().split())
+# array = list(map(int,input().split()))
+
+# count = count_by_range(array, x, x)
+
+# if count == 0:
+#     print(-1)
+# else:
+#     print(count)
+
+# # 고정점 찾기 
+# n = int(input())
+# num = list(map(int,input().split())) 
+
+# def sol(start, end):
+#     if start > end:
+#         return -1 
+    
+#     mid = (start+end) // 2
+
+#     if mid == num[mid]:
+#         return mid
+
+#     elif mid > num[mid]:
+#         return sol(mid + 1, end) 
+#     else:
+#         return sol(start, end -1) 
+
+# print(sol(0,n-1))
 
 
-
-
-
+# 공유기 설치 
+n, c = map(int,input().split())
