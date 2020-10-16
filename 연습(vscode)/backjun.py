f2493986c79215dd48037c4a9145cd204dddb717
GeonWooPaeng <gwpaeng@naver.com>
2020-10-16 15:15:41 +0900

    백준

--- 연습(vscode)/backjun.py
+++ 연습(vscode)/backjun.py
@@ -10010,4 +10010,34 @@
 
 
 # 공유기 설치 
-n, c = map(int,input().split())
+# n, c = map(int,input().split())
+# house = []
+# result = 0
+
+# for _ in range(n):
+#     house.append(int(input())) 
+
+# house.sort() 
+
+# start = house[1] - house[0]
+# end = house[-1] - house[0]
+
+# while (start <= end):
+#     mid = (start+end) // 2 
+#     snode = house[0]
+#     cnt = 1
+
+#     for install in range(1,n):
+#         if house[install] >= snode + mid:
+#             snode = house[install]
+#             cnt += 1 
+
+#     if cnt >= c:
+#         start = mid + 1 
+#         result = mid 
+
+#     else:
+#         end = mid - 1 
+
+# print(result )
+
